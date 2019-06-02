cc9a5d3be0ea31f0daa23d4b16493c790b451889
nginnever <ginneversource@gmail.com>
2019-06-01 17:49:44 -0700

    square floats

---
+++ reference-01-field-arithmetic/kernels/field.cl
@@ -0,0 +1,149 @@
+// FinalityLabs - 2019
+// Arbitrary size prime-field arithmetic library (addmod, submod, mulmod, powmod)
+// Montgomery reduction parameters:
+// B = 2^32 (Because our digits are uint32)
+
+typedef uint uint32;
+typedef ulong uint64;
+typedef struct { uint32 val[LIMBS]; } field;
+
+// Adds `num` to `i`th digit of `res` and propagates carry in case of overflow
+void add_digit(uint32 *res, uint32 num) {
+  uint32 old = *res;
+  *res += num;
+  if(*res < old) {
+    res++;
+    while(++(*(res++)) == 0);
+  }
+}
+
+// Greater than or equal
+bool gte(field a, field b) {
+  for(int i = LIMBS - 1; i >= 0; i--){
+    if(a.val[i] > b.val[i])
+      return true;
+    if(a.val[i] < b.val[i])
+      return false;
+  }
+  return true;
+}
+
+// Equals
+bool eq(field a, field b) {
+  for(int i = 0; i < LIMBS; i++)
+    if(a.val[i] != b.val[i])
+      return false;
+  return true;
+}
+
+// Normal addition
+field add(field a, field b) {
+  uint32 carry = 0;
+  for(int i = 0; i < LIMBS; i++) {
+    uint32 old = a.val[i];
+    a.val[i] += b.val[i] + carry;
+    carry = carry ? old >= a.val[i] : old > a.val[i];
+  }
+  return a;
+}
+
+// Normal subtraction
+field sub(field a, field b) {
+  uint32 borrow = 0;
+  for(int i = 0; i < LIMBS; i++) {
+    uint32 old = a.val[i];
+    a.val[i] -= b.val[i] + borrow;
+    borrow = borrow ? old <= a.val[i] : old < a.val[i];
+  }
+  return a;
+}
+
+// Modular multiplication
+field mulmod(field a, field b) {
+  field p = P; // TODO: Find a solution for this
+
+  // Long multiplication
+  uint32 res[LIMBS * 2] = {0};
+  for(uint32 i = 0; i < LIMBS; i++) {
+    uint32 carry = 0;
+    for(uint32 j = 0; j < LIMBS; j++) {
+      uint64 product = (uint64)a.val[i] * b.val[j] + res[i + j] + carry;
+      res[i + j] = product & 0xffffffff;
+      carry = product >> 32;
+    }
+    res[i + LIMBS] = carry;
+  }
+
+  // Montgomery reduction
+  for(uint32 i = 0; i < LIMBS; i++) {
+    uint64 u = ((uint64)INV * (uint64)res[i]) & 0xffffffff;
+    uint32 carry = 0;
+    for(uint32 j = 0; j < LIMBS; j++) {
+      uint64 product = u * p.val[j] + res[i + j] + carry;
+      res[i + j] = product & 0xffffffff;
+      carry = product >> 32;
+    }
+    add_digit(res + i + LIMBS, carry);
+  }
+
+  // Divide by R
+  field result;
+  for(int i = 0; i < LIMBS; i++) result.val[i] = res[i+LIMBS];
+
+  if(gte(result, p))
+    result = sub(result, p);
+
+  return result;
+}
+
+// Modular negation
+field negmod(field a) {
+  field p = P; // TODO: Find a solution for this
+  return sub(p, a);
+}
+
+// Modular subtraction
+field submod(field a, field b) {
+  field p = P; // TODO: Find a solution for this
+  field res = sub(a, b);
+  if(!gte(a, b)) res = add(res, p);
+  return res;
+}
+
+// Modular addition
+field addmod(field a, field b) {
+  return submod(a, negmod(b));
+}
+
+// Modular exponentiation
+field powmod(field base, uint32 exponent) {
+  field res = ONE;
+  while(exponent > 0) {
+    if (exponent & 1)
+      res = mulmod(res, base);
+    exponent = exponent >> 1;
+    base = mulmod(base, base);
+  }
+  return res;
+}
+
+field powmodcached(__global field *bases, uint32 exponent) {
+  field res = ONE;
+  uint32 i = 0;
+  while(exponent > 0) {
+    if (exponent & 1)
+      res = mulmod(res, bases[i]);
+    exponent = exponent >> 1;
+    i++;
+  }
+  return res;
+}
+
+uint32 bitreverse(uint32 n, uint32 bits) {
+  uint32 r = 0;
+  for(int i = 0; i < bits; i++) {
+    r = (r << 1) | (n & 1);
+    n >>= 1;
+  }
+  return r;
+}
--- reference-01-field-arithmetic/libff/main.cpp
+++ reference-01-field-arithmetic/libff/main.cpp
@@ -9,6 +9,11 @@
 
 using namespace libff;
 
+#define DATA_SIZE (131072)
+
+#include <unistd.h>
+char *getcwd(char *buf, size_t size);
+
 void write_mnt4_fq(FILE* output, Fq<mnt4753_pp> x) {
   fwrite((void *) x.mont_repr.data, libff::mnt4753_q_limbs * sizeof(mp_size_t), 1, output);
 }
@@ -37,10 +42,32 @@ int main(int argc, char *argv[])
     // argv should be
     // { "main", "compute", inputs, outputs }
     printf("Running mul on inputs... %s\n", argv[2]);
-    dispatch_queue_t queue =
-               gcl_create_dispatch_queue(CL_DEVICE_TYPE_GPU, NULL);
+    char cwd[1024];
+    if (getcwd(cwd, sizeof(cwd)) != NULL) {
+       printf("Current working dir: %s\n", cwd);
+    } else {
+       perror("getcwd() error");
+       return 1;
+    }
+
+    FILE *fp;
+    char *source_str;
+    size_t source_size, program_size;
+
+    fp = fopen("../../kernels/square.cl", "r");
+    if (!fp) {
+        fprintf(stderr, "could not open program file\n");
+        exit(1);
+    }
+
+    char* program_source_code;
+    size_t program_source_code_size;
+    program_source_code = (char*)malloc(400000);
+    program_source_code_size = fread(program_source_code, 1, 400000, fp);
+    fclose(fp);
 
     int err;                            // error code returned from api calls
+    char name[128];
       
     float data[DATA_SIZE];              // original data set given to device
     float results[DATA_SIZE];           // results returned from device
@@ -48,12 +75,172 @@ int main(int argc, char *argv[])
 
     size_t global;                      // global domain size for our calculation
     size_t local;                       // local domain size for our calculation
+    cl_device_id device_id;             // compute device id 
+    cl_context context;                 // compute context
+    cl_command_queue commands;          // compute command queue
+    cl_program program;                 // compute program
+    cl_kernel kernel;                   // compute kernel
     
-    cl_device_id gpu = gcl_get_device_id_with_dispatch_queue(queue);
-    printf("Device id: %u\n", gpu);
+    cl_mem input;                       // device memory used for the input array
+    cl_mem output;                      // device memory used for the output array
 
-    clGetDeviceInfo(gpu, CL_DEVICE_NAME, 128, name, NULL);
+    // Fill our data set with random float values
+    //
+    unsigned int count = DATA_SIZE;
+    int i = 0;
+    for(i = 0; i < count; i++)
+        data[i] = rand() / (float)RAND_MAX;
+    
+    // Connect to a compute device
+    //
+    int gpu = 1;
+    err = clGetDeviceIDs(NULL, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device_id, NULL);
+    if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to create a device group!\n");
+        return EXIT_FAILURE;
+    }
+
+    printf("Device id: %u\n", device_id);
+
+    clGetDeviceInfo(device_id, CL_DEVICE_NAME, 128, name, NULL);
     fprintf(stdout, "Created a dispatch queue using the %s\n", name);
+
+    // Create a compute context 
+    //
+    context = clCreateContext(0, 1, &device_id, NULL, NULL, &err);
+    if (!context)
+    {
+        printf("Error: Failed to create a compute context!\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create a command commands
+    //
+    commands = clCreateCommandQueue(context, device_id, 0, &err);
+    if (!commands)
+    {
+        printf("Error: Failed to create a command commands!\n");
+        return EXIT_FAILURE;
+    }
+
+    // Create the compute program from the source buffer
+    //
+    program = clCreateProgramWithSource(context, 1, (const char **) &program_source_code, &program_source_code_size, &err);
+    if (!program)
+    {
+        printf("Error: Failed to create compute program!\n");
+        return EXIT_FAILURE;
+    }
+
+    // Build the program executable
+    //
+    err = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+    if (err != CL_SUCCESS)
+    {
+        size_t len;
+        char buffer[2048];
+
+        printf("Error: Failed to build program executable!\n");
+        clGetProgramBuildInfo(program, device_id, CL_PROGRAM_BUILD_LOG, sizeof(buffer), buffer, &len);
+        printf("%s\n", buffer);
+        exit(1);
+    }
+
+    // Create the compute kernel in the program we wish to run
+    //
+    kernel = clCreateKernel(program, "square", &err);
+    if (!kernel || err != CL_SUCCESS)
+    {
+        printf("Error: Failed to create compute kernel!\n");
+        exit(1);
+    }
+
+    // Create the input and output arrays in device memory for our calculation
+    //
+    input = clCreateBuffer(context,  CL_MEM_READ_ONLY,  sizeof(float) * count, NULL, NULL);
+    output = clCreateBuffer(context, CL_MEM_WRITE_ONLY, sizeof(float) * count, NULL, NULL);
+    if (!input || !output)
+    {
+        printf("Error: Failed to allocate device memory!\n");
+        exit(1);
+    }
+
+    // Write our data set into the input array in device memory 
+    //
+    err = clEnqueueWriteBuffer(commands, input, CL_TRUE, 0, sizeof(float) * count, data, 0, NULL, NULL);
+    if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to write to source array!\n");
+        exit(1);
+    }
+
+    // Set the arguments to our compute kernel
+    //
+    err = 0;
+    err  = clSetKernelArg(kernel, 0, sizeof(cl_mem), &input);
+    err |= clSetKernelArg(kernel, 1, sizeof(cl_mem), &output);
+    err |= clSetKernelArg(kernel, 2, sizeof(unsigned int), &count);
+    if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to set kernel arguments! %d\n", err);
+        exit(1);
+    }
+
+    // Get the maximum work group size for executing the kernel on the device
+    //
+    err = clGetKernelWorkGroupInfo(kernel, device_id, CL_KERNEL_WORK_GROUP_SIZE, sizeof(local), &local, NULL);
+    if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to retrieve kernel work group info! %d\n", err);
+        exit(1);
+    }
+
+    printf("Max work size: %u\n", local);
+
+    // Execute the kernel over the entire range of our 1d input data set
+    // using the maximum number of work group items for this device
+    //
+    global = count;
+    err = clEnqueueNDRangeKernel(commands, kernel, 1, NULL, &global, &local, 0, NULL, NULL);
+    if (err)
+    {
+        printf("Error: Failed to execute kernel!\n");
+        return EXIT_FAILURE;
+    }
+
+    clFinish(commands);
+
+    // Read back the results from the device to verify the output
+    //
+    err = clEnqueueReadBuffer( commands, output, CL_TRUE, 0, sizeof(float) * count, results, 0, NULL, NULL );  
+    if (err != CL_SUCCESS)
+    {
+        printf("Error: Failed to read output array! %d\n", err);
+        exit(1);
+    }
+
+    // Validate our results
+    //
+    correct = 0;
+    for(i = 0; i < count; i++)
+    {
+        if(results[i] == data[i] * data[i])
+            correct++;
+    }
+    
+    // Print a brief summary detailing the results
+    //
+    printf("Computed '%d/%d' correct values!\n", correct, count);
+    
+    // Shutdown and cleanup
+    //
+    clReleaseMemObject(input);
+    clReleaseMemObject(output);
+    clReleaseProgram(program);
+    clReleaseKernel(kernel);
+    clReleaseCommandQueue(commands);
+    clReleaseContext(context);
 
     mnt4753_pp::init_public_params();
     mnt6753_pp::init_public_params();
